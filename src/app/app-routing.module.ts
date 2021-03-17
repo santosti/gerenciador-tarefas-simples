f5f39d799d7e95abe0cff32083a9d3d42a626975
Simone Antunes <sanntosti@gmail.com>
2021-03-16 22:24:55 -0300

    Preparando rota listar e inserido visual do campo de tarefa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
-import { NgModule} from '@angular/core';
-import { Routes, RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TarefaRoutes } from './tarefas';
 
 export const routes: Routes = [
-
+  {
+    path: '',
+    redirectTo: '/tarefas/listar',
+    pathMatch: 'full',
+  },
+  ...TarefaRoutes,
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-
 export class AppRoutingModule {}
